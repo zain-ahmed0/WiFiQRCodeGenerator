34db358fcf4c04137577d05cfa357e2af9cbd8cd
Zain Ahmed <zain.auk05@gmail.com>
2026-07-24 23:36:31 +0100

    chore: fix syntax

--- src/WiFiQRCodeGenerator/Models/WiFiCredentials.cs
+++ src/WiFiQRCodeGenerator/Models/WiFiCredentials.cs
@@ -3,6 +3,6 @@ namespace WifiQRCodeGenerator.Models;
 public class WiFiCredentials(string name, string password)
 {
     public string? Name { get; set; }
-    
+
     public string? Password { get; set; }
 }
--- src/WiFiQRCodeGenerator/Services/QrCodeService.cs
+++ src/WiFiQRCodeGenerator/Services/QrCodeService.cs
@@ -7,11 +7,12 @@ public static class QrCodeService
 {
     public static byte[] Generate(WiFiCredentials credentials)
     {
-        var wifiPayload = new PayloadGenerator.WiFi(credentials.Name, credentials.Password, PayloadGenerator.WiFi.Authentication.WPA2);
+        var wifiPayload = new PayloadGenerator.WiFi(credentials.Name, credentials.Password,
+            PayloadGenerator.WiFi.Authentication.WPA2);
         var qrCodeData = QRCodeGenerator.GenerateQrCode(wifiPayload, QRCodeGenerator.ECCLevel.Q);
-            
+
         using var pngRenderer = new PngByteQRCode(qrCodeData);
-            
+
         return pngRenderer.GetGraphic(20);
     }
 }
