fd7a06241c5384a9823b7b6a53a584b3dbe78b45
Zain Ahmed <zain.auk05@gmail.com>
2026-07-24 23:50:09 +0100

    refactor: refactor wificredentials model

--- src/WiFiQRCodeGenerator/Models/WiFiCredentials.cs
+++ src/WiFiQRCodeGenerator/Models/WiFiCredentials.cs
@@ -1,8 +1,3 @@
 namespace WifiQRCodeGenerator.Models;
 
-public class WiFiCredentials(string name, string password)
-{
-    public string? Name { get; set; }
-
-    public string? Password { get; set; }
-}
+public record WiFiCredentials(string Name, string Password);
