24c37fc4af2f886e8129e0cd9cac0bfe1d10649a
Zain Ahmed <zain.auk05@gmail.com>
2026-07-25 01:35:40 +0100

    feat: add publish to homebrew workflow

---
+++ homebrew/wqr.rb
@@ -0,0 +1,23 @@
+class Wqr < Formula
+  desc "Generate a QR code for your WiFi network"
+  homepage "https://github.com/zain-ahmed0/WiFiQRCodeGenerator"
+  version "1.0.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/zain-ahmed0/WiFiQRCodeGenerator/releases/download/v1.0.0/wqr-osx-arm64.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/zain-ahmed0/WiFiQRCodeGenerator/releases/download/v1.0.0/wqr-osx-x64.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "wqr"
+  end
+
+  test do
+    system "#{bin}/wqr", "--help"
+  end
+end
